9c73e0f562e2d0f722cc5e825a2a9eaa3b99fb63
root <root@pixelboard2.(none)>
2014-11-15 01:23:10 -0500

    add sinmple server install

---
+++ server/install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+echo 'ln -sf /usr/local/src/pixelboard/server/pixelboard-server.sh /usr/local/bin/pixelboard-server'
+ln -sf /usr/local/src/pixelboard/server/pixelboard-server.sh /usr/local/bin/pixelboard-server
+
+echo 'cp /usr/local/src/pixelboard/server/pixelboard-server.init.d /etc/init.d/pixelboard-server'
+cp /usr/local/src/pixelboard/server/pixelboard-server.init.d /etc/init.d/pixelboard-server
+
+echo 'update-rc.d pixelboard-server defaults'
+update-rc.d pixelboard-server defaults
