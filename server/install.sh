50d3ce6dce34ff2dcb54f98873be1fd20b6d643a
root <root@pixelboard1.(none)>
2014-11-15 01:36:02 -0500

    added server restart to install

--- server/install.sh
+++ server/install.sh
@@ -4,8 +4,13 @@
 echo 'ln -sf /usr/local/src/pixelboard/server/pixelboard-server.sh /usr/local/bin/pixelboard-server'
 ln -sf /usr/local/src/pixelboard/server/pixelboard-server.sh /usr/local/bin/pixelboard-server
 
+echo
 echo 'cp /usr/local/src/pixelboard/server/pixelboard-server.init.d /etc/init.d/pixelboard-server'
 cp /usr/local/src/pixelboard/server/pixelboard-server.init.d /etc/init.d/pixelboard-server
 
+echo
 echo 'update-rc.d pixelboard-server defaults'
 update-rc.d pixelboard-server defaults
+
+echo
+service pixelboard-server restart
